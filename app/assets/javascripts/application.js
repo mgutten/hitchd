962bd7b6fb0b767ef5af50d79a7f0969765d6861
Marshall Guttenberg <mguttenberg@healthright.com>
2015-12-29 00:36:03 -0800

    remove turbolinks to force reload of javascript on each page, mobile nav menu

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,5 +12,5 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require turbolinks
+//= require jquery.slicknav.min.js
 //= require_tree .
---
+++ app/assets/javascripts/nav.js
@@ -0,0 +1,32 @@
+(function($) {
+
+  var nav = {
+    init: function() {
+      this.$nav = $( 'nav' );
+
+      if ( this.mobile() )
+        this.disableDropdown();
+
+      this.slickNav();
+    },
+
+    mobile: function() {
+      return $(window).width() <= 600;
+    },
+
+    disableDropdown: function() {
+      this.$nav.find( '.dropdown, .dropdown-menu' ).removeClass( 'dropdown dropdown-menu' );
+    },
+
+    slickNav: function() {
+      this.$nav.slicknav({
+        label: ''
+      });
+    }
+  };
+
+  $( function() {
+    nav.init();
+  });
+
+})( jQuery )
