d27d5b9f8c92389f4be776b75689c1f8ff5a44fb
Marshall Guttenberg <mguttenberg@healthright.com>
2016-08-27 12:40:37 -0700

    night before celebrations!

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,22 @@ module ApplicationHelper
   def section(title)
     "<hr/><h2>#{title}</h2><hr/>".html_safe
   end
+
+  def calendar(start, selected)
+    start = DateTime.new(2016, 9, start)
+    html = "<div class='calendar'>"
+
+    (0..3).each do |i|
+      date = start + i.days
+      classy = selected.to_s == date.strftime('%d') ? 'selected' : ''
+
+      html << "<div class='day #{classy}'>
+                  <p class='day'>#{date.strftime('%d')}</p>
+                  <p class='of-week'>#{date.strftime('%a')[0..1]}</p>
+              </div>"
+    end
+
+    html << "</div>"
+    html.html_safe
+  end
 end
